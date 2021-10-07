61a84e023a2df8bac49273fe17477149d6d84552
Pavel <stodumoff@yandex.ru>
2021-10-07 23:15:48 +0300

    task_2 update

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -21,13 +21,50 @@
 А если вы знаете как через Python работать с БД, привяжите к заданию БД и сохраняйте хеши там.
 """
 import hashlib
+import sqlite3
 
-salt = 'salt'
-password = input('Введите пароль: ')
-pas = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
-print(pas)
-password_t = input('Введите пароль ещё раз для проверки: ')
-if hashlib.sha256(salt.encode() + password_t.encode()).hexdigest() == pas:
-    print('Вы ввели правильный пароль')
-else:
-    print('Вы ввели неправильный пароль')
+try:
+    connection = sqlite3.connect('pass_db')
+    cursor = connection.cursor()
+    # cursor.execute('''DROP TABLE IF EXISTS passwords''')
+    cursor.execute('CREATE TABLE IF NOT EXISTS passwords ('
+                   'password TEXT);')
+    insert_password = '''INSERT INTO passwords (password) VALUES (?);'''
+
+    salt = input('Введите логин: ')
+    password = input('Введите пароль: ')
+    pas = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+    cursor.execute('''SELECT password FROM passwords''')
+    if (pas,) not in cursor.fetchall():
+        cursor.execute(insert_password, (pas,))
+
+    # cursor.execute('''SELECT password FROM passwords''')
+    # print(cursor.fetchall()) # список паролей, хранящихся в БД
+
+    connection.commit()
+
+    password = input(f'Введите пароль ещё раз для логина {salt}: ')
+    cursor.execute('''SELECT password FROM passwords''')
+    if (hashlib.sha256(salt.encode() + password.encode()).hexdigest(),) in cursor.fetchall():
+        print('Вы ввели правильный пароль')
+    else:
+        print('Вы ввели неправильный пароль')
+
+    cursor.close()
+except Exception as ex:
+    print(ex)
+
+finally:
+    connection.close()
+
+'''Попробовал с БД. Пришлось соль сделать ввиде логина через ввод, иначе БД накапливала пароли, и в случае если первый раз вводишь новый пароль, а повторно какой-нибудь из тех, что уже есть в базе, ответ получается утвердительный, хотя пароли вводишь разные'''
+
+# salt = 'salt'
+# password = input('Введите пароль: ')
+# pas = hashlib.sha256(salt.encode() + password.encode()).hexdigest()
+#     print(pas, type(pas))
+# password_t = input('Введите пароль ещё раз для проверки: ')
+# if hashlib.sha256(salt.encode() + password_t.encode()).hexdigest() == pas:
+#     print('Вы ввели правильный пароль')
+# else:
+#     print('Вы ввели неправильный пароль')
