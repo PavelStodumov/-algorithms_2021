98810456ef1a13c10df56771f8b710c40231f782
Pavel <stodumoff@yandex.ru>
2021-10-11 18:12:44 +0300

    task_3 compleate

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -13,6 +13,8 @@
 
 Без аналитики задание считается не принятым
 """
+from timeit import timeit
+from cProfile import run
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +39,31 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+def revers_4(enter_num):
+    return ''.join(reversed(str(enter_num)))
+
+
+num = 123456789
+
+print('#' * 30 + 'revers_1(num)' + '#' * 30)
+run('revers_1(num)')
+print(timeit('revers_1(num)', globals=globals()))
+
+print('#' * 30 + 'revers_2(num)' + '#' * 30)
+run('revers_2(num)')
+print(timeit('revers_2(num)', globals=globals()))
+
+print('#' * 30 + 'revers_3(num)' + '#' * 30)
+run('revers_3(num)')
+print(timeit('revers_3(num)', globals=globals()))
+
+print('#' * 30 + 'revers_4(num)' + '#' * 30)
+run('revers_4(num)')
+print(timeit('revers_4(num)', globals=globals()))
+
+'''revers_1 реализована через рекурсию. Самое неэффективное решение. для 9-значного числа вызывается 13 функций
+revers_2 цикл выполняет ряд математическихопераций на каждой итерации. 
+revers_3 срез строки, самое быстрое решение с использованием встроенных методов работы со строками.
+revers_4 реализовано с помощью встроенной функции, быстрее чем цикл, но медленнее среза.
+'''
