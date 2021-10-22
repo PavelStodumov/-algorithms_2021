c622c84b787e2c0c0b01a4d28ac14156f7b97da9
Pavel <stodumoff@yandex.ru>
2021-10-22 21:12:41 +0300

    compleate

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -40,3 +40,52 @@ for i in
 
 
 """
+from random import randint
+from timeit import timeit
+
+# m = int(input())
+m = 10
+my_list = [randint(0, 100) for _ in range(2 * m + 1)]
+
+
+def med1(ls):
+    c = len(ls) // 2
+    while True:
+        left = []
+        right = []
+        for i in ls[:c]:
+            if i < ls[c]:
+                left.append(i)
+            else:
+                right.append(i)
+        for i in ls[c + 1:]:
+            if i < ls[c]:
+                left.append(i)
+            else:
+                right.append(i)
+        if len(left) == len(right):
+            return f'медиана: {ls[c]}\n{ls}'
+        ls = left + [ls[c]] + right
+
+
+'''Идея такая: брать число из середины, остальной массив делить на два: в одном числа меньше взятого, в другом - меньше.
+Если подмассивы разной длинны то складываем их и повторяем процедуру.'''
+
+
+def med2(ls):
+    c = len(ls) // 2
+    i = 1
+    while i < len(ls):
+        if ls[i - 1] <= ls[i]:
+            i += 1
+        else:
+            ls[i - 1], ls[i] = ls[i], ls[i - 1]
+            if i > 1:
+                i -= 1
+    return f'медиана: {ls[c]}\n{ls}'
+'''С гномьей сортировкой'''
+
+print(med1(my_list))
+print(f'Время выполнения моего цикла: {timeit("med1(my_list)", globals=globals(), number=10000)}')
+print(med2(my_list))
+print(f'Время выполнения гномьей сортировки: {timeit("med2(my_list)", globals=globals(), number=10000)}')
